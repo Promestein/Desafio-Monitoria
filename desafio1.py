5e015982f5e38b26df0beb9181d658d12d058953
Gabriel Costa <55096273+Promestein@users.noreply.github.com>
2021-04-06 12:00:15 -0300

    Create desafio1.py

---
+++ desafio1.py
@@ -0,0 +1,20 @@
+"""
+Faça um programa que receba um inteiro e imprima
+um padrão como o do exemplo, para qualquer número
+de 1 a 9.
+Exemplo: Entrada  Saída
+            5       1
+                   2 2
+                  3 3 3
+                 4 4 4 4
+                5 5 5 5 5
+            9           1
+                       2 2
+                      3 3 3
+                     4 4 4 4
+                    5 5 5 5 5
+                   6 6 6 6 6 6
+                  7 7 7 7 7 7 7
+                 8 8 8 8 8 8 8 8
+                9 9 9 9 9 9 9 9 9
+"""
